cab552ae6f80921d13ece73ee7f28715333288b4
unknown <dhiaajhamed@gmail.com>
2021-03-20 20:05:38 +0300

    modified scripts at package.json, and made config available

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Home file for configuration options
+ * 
+ */
+
+// dependencies
+
+// container
+var config = {};
+
+config.token = "NDg2ODAwNzc5MjE5NDM1NTMw.W4-Gjg.z6tb-lOyRzGBCLQQk6jL0T06p-o";
+
+
+module.exports = config;
